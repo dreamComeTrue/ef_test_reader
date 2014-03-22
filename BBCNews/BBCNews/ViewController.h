21c51a6adf3af7462d1642b9903e03623b933a6c
Tdr <yakimenkop@gmail.com>
2014-03-22 15:19:39 +0300

    Initial
    Empty project created

---
+++ BBCNews/BBCNews/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BBCNews
+//
+//  Created by Pavel Yakimenko on 3/22/14.
+//  Copyright (c) 2014 Pavel Yakimenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
